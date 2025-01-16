4a12972b952f295cf51bf2373f4fee546eeefc57
Alejandro E. Aguilar <alezek@gmail.com>
2025-01-16 13:24:28 -0300

    First commit. Configurando Entorno.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Conversor de Moneda
+
+Uno de los desafíos del programa ONE de Oracle.
+
+Work in progress...even this readme !!!
+
---
+++ src/ar/com/alezek/currencyconverter/principal/Principal.java
@@ -0,0 +1,8 @@
+package ar.com.alezek.currencyconverter.principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola Currency Converter !!!");
+    }
+}
+
