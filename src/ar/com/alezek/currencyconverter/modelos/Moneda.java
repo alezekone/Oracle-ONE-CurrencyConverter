5960a7b31e007320357e4f3219aef004ded66ff6
Alejandro E. Aguilar <alezek@gmail.com>
2025-01-17 13:07:38 -0300

    + Agrego clase (enum) Moneda, para representar las monedas a tratar.

---
+++ src/ar/com/alezek/currencyconverter/modelos/Moneda.java
@@ -0,0 +1,11 @@
+package ar.com.alezek.currencyconverter.modelos;
+
+// Siglas de acuerdo al standard ISO 4217:
+// USD US Dollar
+// ARS Peso Argentino
+// BRL Real Brasileño
+// CLP Peso Chileno
+
+public enum Moneda {
+    USD, ARS, BRL, CLP
+}
