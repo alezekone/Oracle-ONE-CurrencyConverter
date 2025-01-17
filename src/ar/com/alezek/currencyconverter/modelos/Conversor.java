dc30449ad956ccb9253f14ff9cce39aeae361a14
Alejandro E. Aguilar <alezek@gmail.com>
2025-01-17 19:48:57 -0300

    Agrego el modificador 'transient' a algunos atributos para que Gson no los incluya en la serialización.

--- src/ar/com/alezek/currencyconverter/modelos/Conversor.java
+++ src/ar/com/alezek/currencyconverter/modelos/Conversor.java
@@ -1,20 +1,33 @@
 package ar.com.alezek.currencyconverter.modelos;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Conversor {
     String origen;
     String destino;
-    double tipoDeCambio;
+    double tipoDeCambio = 0;
+    transient URI direccion;
+    transient HttpClient client;
+    transient HttpRequest request;
+    transient Gson gson;
 
-    public Conversor(String origen, String destino, double tipoDeCambio) {
-        this.origen = origen;
-        this.destino = destino;
-        this.tipoDeCambio = tipoDeCambio;
+    public Conversor(Moneda origen, Moneda destino) {
+        this.origen = origen.name();
+        this.destino = destino.name();
+        // this.tipoDeCambio = tipoDeCambio;
     }
 
-    public Conversor(ConversorFromExchangeRateAPI conversion) {
-        this.origen = conversion.baseCode();
-        this.destino = conversion.targetCode();
-        this.tipoDeCambio = conversion.conversionRate();
+    public Conversor(ConversorFromExchangeRateAPI conversor) {
+        this.origen = conversor.baseCode();
+        this.destino = conversor.targetCode();
+        this.tipoDeCambio = conversor.conversionRate();
     }
 
     public String getOrigen() {
@@ -36,6 +49,53 @@ public class Conversor {
                 ", destino='" + destino + '\'' +
                 ", tipoDeCambio=" + tipoDeCambio +
                 '}';
+    }
+
+    public void inicializar(){
+        this.direccion = URI.create("https://v6.exchangerate-api.com/v6/1983c3cae5adae371dcacfce/pair/"+origen+"/"+destino);
+
+        this.gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .setPrettyPrinting()
+                .create();
+
+        this.client = HttpClient.newHttpClient();
+        this.request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+    }
+
+    public double convertir(double importeEnMonedaOrigen){
+        double importeEnMonedaDestino = 0;
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Respuesta obtenida:" + response.body());
+
+            String jsonResponse = response.body();
+
+            ConversorFromExchangeRateAPI miConversorFromExchangeRateAPI = gson.fromJson(jsonResponse, ConversorFromExchangeRateAPI.class);
+            System.out.println("Respuesta obtenida, i.e. estoy imprimiendo un objeto (implícitamente estoy usando toString()): ");
+            System.out.println(miConversorFromExchangeRateAPI);
+
+            Conversor miConversor = new Conversor(miConversorFromExchangeRateAPI);
+            System.out.println("Ahora imprimiré el objeto, como lo quiero para operar con él:");
+            System.out.println(miConversor);
+            this.tipoDeCambio = miConversor.getTipoDeCambio();
+
+            System.out.println("Ahora serializo el objeto con toJson para ver como quedaría si quisiera enviarlo:");
+            System.out.println(gson.toJson(miConversor, Conversor.class));
+            System.out.println("Lo anterior es interesante. Vemos que Gson efectivamente usó la FieldNamePolicy, pero...");
+            System.out.println("lo hizo sobre nuestro objeto de clase 'miConversor', como es lógico, porque no sabe del DTO.");
+            System.out.println("Supongo entonces que, si quisiera enviar mi objeto (si fuera una posibilidad, que no lo es) de regreso");
+            System.out.println("a ExchangeRate-API, tendría que primero convertirlo a mi DTO, y luego serializar ese DTO con GSON. ");
+
+            return importeEnMonedaOrigen*this.getTipoDeCambio();
+
+        } catch (Exception e) {
+            throw new RuntimeException("La conversión no fue posible." + e.getMessage());
+        }
     }
 
 }
--- src/ar/com/alezek/currencyconverter/principal/Principal.java
+++ src/ar/com/alezek/currencyconverter/principal/Principal.java
@@ -11,48 +11,16 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static ar.com.alezek.currencyconverter.modelos.Moneda.*;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola Currency Converter !!!");
 
-        String origen = "USD";
-        String destino = "ARS";
-        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/1983c3cae5adae371dcacfce/pair/"+origen+"/"+destino);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .setPrettyPrinting()
-                .create();
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(direccion)
-                .build();
-
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Respuesta obtenida:" + response.body());
-
-            String jsonResponse = response.body();
-
-            ConversorFromExchangeRateAPI miConversorFromExchangeRateAPI = gson.fromJson(jsonResponse, ConversorFromExchangeRateAPI.class);
-            System.out.println("Respuesta obtenida, i.e. estoy imprimiendo un objeto (implícitamente estoy usando toString()): ");
-            System.out.println(miConversorFromExchangeRateAPI);
-
-            Conversor miConversor = new Conversor(miConversorFromExchangeRateAPI);
-            System.out.println("Ahora imprimiré el objeto, como lo quiero para operar con él:");
-            System.out.println(miConversor);
-
-            System.out.println("Ahora serializo el objeto con toJson para ver como quedaría si quisiera enviarlo:");
-            System.out.println(gson.toJson(miConversor, Conversor.class));
-            System.out.println("Lo anterior es interesante. Vemos que Gson efectivamente usó la FieldNamePolicy, pero...");
-            System.out.println("lo hizo sobre nuestro objeto de clase 'miConversor', como es lógico, porque no sabe del DTO.");
-            System.out.println("Supongo entonces que, si quisiera enviar mi objeto (si fuera una posibilidad, que no lo es) de regreso");
-            System.out.println("a ExchangeRate-API, tendría que primero convertirlo a mi DTO, y luego serializar ese DTO con GSON. ");
-
-        } catch (Exception e) {
-            throw new RuntimeException("La conversión no fue posible." + e.getMessage());
-        }
+        Conversor miConversor = new Conversor(USD, ARS);
+        miConversor.inicializar();
+        double importeEnMonedaDestino = miConversor.convertir(100.00);
+        System.out.println("100 USD equivalen a " + importeEnMonedaDestino + " pesos argentinos.");
 
     }
 }
